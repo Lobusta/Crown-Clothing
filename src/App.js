bc82b9611c5e9aa16cc8009a1bdd5f71669a08a4
Lobusta <sajid.ahmad.1731998@gmail.com>
2022-05-22 23:53:07 +0500

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my man
+          Hello my man how you doing
+          
         </p>
         <a
           className="App-link"
